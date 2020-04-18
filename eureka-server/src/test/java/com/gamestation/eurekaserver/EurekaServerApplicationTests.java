13cb610accd150cba26a11eddb9ab97d2d97b869
Spider <523995513@qq.com>
2020-04-18 14:56:04 +0800

    new eurekaServr

---
+++ eureka-server/src/test/java/com/gamestation/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamestation.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
